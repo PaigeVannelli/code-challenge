302446746be5f89d2153bb6c37db395a5d15475c
Paige Vannelli <paigevannelli@gmail.com>
2021-07-20 16:51:04 -0600

    Delete commented out lines

--- main.js
+++ main.js
@@ -41,18 +41,3 @@ const loadTherapists = () => {
 }
 
 window.addEventListener('load', loadTherapists)
-
-// In order to evaluate some of your skills that will be necessary for this position, 
-// please complete the coding assignment below. Please only use pure JavaScript (no jQuery, react, or similar libraries/frameworks).
-
-// Using any IDE or platform you would like, please consume the JSON data provided 
-// and display it as pictured below. You may submit raw .txt files. You may also 
-// submit a GitHub page from your repo with the solution as long as the page is deployed.
-
-// Your display must include the following features:
-
-// List each user, last name first, with each name capped
-// When a user is hovered over, the containing element must change color
-// Include a padding of 10px by 5px for each user
-// The display must be vertically and horizontally centered on the screen, and must remain so during screen resizing
-
